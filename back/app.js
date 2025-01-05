451bf9451d11ec2ba69c9a6847f19fde95dfc9fd
DianaOprea17 <diana.oprea1705@gmail.com>
2025-01-05 13:54:53 +0200

    setup2

--- back/app.js
+++ back/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 const port = 3000;
+
+const app = express();
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors())
+
 
 app.get('/', (req, res) => {
   res.send('MOVIE APP!');
